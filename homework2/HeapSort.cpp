85be06b54b301d2bec3f0ced7bb73894c3811e22
aragami3070 <smirnov17612@gmail.com>
2025-02-16 12:48:33 +0400

    Add: heap sort

--- homework2/HeapSort.cpp
+++ homework2/HeapSort.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
 #include <vector>
 
+void HeapShifting(std::vector<int>& array, int index, int size) {
+    while (true) {
+        // Think index elem is max
+        int indexMax = index;
+
+        // Set index childs
+        int leftChild = 2 * index + 1;
+        int rightChild = 2 * index + 2;
+
+        // If exists left child and he > parent
+        if (leftChild < size && array[indexMax] < array[leftChild]) {
+            // Then change indexMax to leftChild
+            indexMax = leftChild;
+        }
+        // If exists rightChild child and he > parent
+        if (rightChild < size && array[indexMax] < array[rightChild]) {
+            // Then change indexMax to rightChild
+            indexMax = rightChild;
+        }
+
+        // If index max dont change
+        if (indexMax == index) {
+            // End
+            return;
+        }
+        // Else swap child and parent
+        else {
+            // Swap
+            int temp = array[index];
+            array[index] = array[indexMax];
+            array[indexMax] = temp;
+
+            // index = new parent
+            index = indexMax;
+        }
+    }
+}
+
+void HeapSort(std::vector<int>& array, int size) {
+    for (int i = (size / 2) + 1; i >= 0; i--) {
+        HeapShifting(array, i, size);
+    }
+
+    for (int j = size - 1; j > 0; j--) {
+        int temp = array[0];
+        array[0] = array[j];
+        array[j] = temp;
+
+        HeapShifting(array, 0, j);
+    }
+}
+
 
 int main() {
     // Initialize array
@@ -26,4 +78,11 @@ int main() {
     }
 
     // Sort array
+    HeapSort(array, size);
+
+    // Output array
+    std::cout << "Результат:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
+    }
 }
