68bc87ef8b7f92312aec3bfbfb54dad4c74ab7cd
aragami3070 <smirnov17612@gmail.com>
2025-04-17 00:16:51 +0400

    Add: initial AVLTree defenition

---
+++ homework5/AVLTree/AVLTree.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+template <typename T> class AVLNode {
+  public:
+    T Key;
+    AVLNode *Left;
+    AVLNode *Right;
+    int Height;
+
+    AVLNode(T k) : Key(k), Left(nullptr), Right(nullptr), Height(1) {
+    }
+};
+
+template <typename T> class AVLTree {
+  private:
+    AVLNode<T> *root;
+
+    int Height(AVLNode<T> *node);
+    int BalanceFactor(AVLNode<T> *node);
+    AVLNode<T> *RightRotate(AVLNode<T> *y);
+    AVLNode<T> *LeftRotate(AVLNode<T> *x);
+    AVLNode<T> *Insert(AVLNode<T> *node, T key);
+    AVLNode<T> *MinValueNode(AVLNode<T> *node);
+    AVLNode<T> *DeleteNode(AVLNode<T> *root, T key);
+    void PrintHelper(AVLNode<T> *root, std::string indent, bool last);
+
+  public:
+    AVLTree() : root(nullptr) {
+    }
+
+    void Insert(T key);
+    void Remove(T key);
+    void Print();
+};
