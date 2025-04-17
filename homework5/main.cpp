654ecb367a8c47a8a2737176be7408416ba15edf
aragami3070 <smirnov17612@gmail.com>
2025-04-18 00:54:15 +0400

    Add: main

--- homework5/main.cpp
+++ homework5/main.cpp
@@ -1,1 +1,14 @@
 #include "AVLTree/AVLTree.h"
+
+int main() {
+    AVLTree<int> tree;
+    int n, x;
+    std::cout << "Введите размер дерева:" << std::endl;
+    std::cin >> n;
+    std::cout << "Введите дерево:" << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cin >> x;
+        tree.Insert(x);
+    }
+    tree.Print();
+}
